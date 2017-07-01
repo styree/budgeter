86cb60feb5c0600d191c5e737482dfc4d7273387
Steve Tyree <hello@stevetyree.com>
2017-07-01 17:05:24 -0600

    Initial Commit

---
+++ app/index.js
@@ -0,0 +1,10 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var App = require('./components/App');
+
+// ReactDOM.render(<App/>, document.getElementById('app'));
+
+ReactDOM.render(
+	<h1>Hello, world!</h1>,
+	document.getElementById('root')
+);
