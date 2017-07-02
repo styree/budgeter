7fedc4727220c3019c3ec5060e6b6e810ad9cb42
Steve Tyree <hello@stevetyree.com>
2017-07-01 23:04:01 -0600

    Basic Input Components

--- app/components/App.js
+++ app/components/App.js
@@ -2,6 +2,11 @@ import React from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RaisedButton from 'material-ui/RaisedButton';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import AppBar from 'material-ui/AppBar';
+import { RadioButton, RadioButtonGroup } from 'material-ui/RadioButton';
+import DatePicker from 'material-ui/DatePicker';
+import InputComponent from './InputComponent/InputComponent.jsx';
+
 
 injectTapEventPlugin();
 
@@ -11,7 +16,26 @@ export default class App extends React.Component {
 		return (
 			<MuiThemeProvider>
 				<div>
-					<h1>Hello World!</h1>
+					<AppBar title="Hello World!"/>
+
+					<DatePicker
+						hintText="Portrait Dialog"
+						defaultDate={new Date()}
+					/>
+
+					<RadioButtonGroup name="type" labelPosition="right" defaultSelected="out">
+						<RadioButton
+							value="out"
+							label="Spending"
+						/>
+						<RadioButton
+							value="in"
+							label="Income"
+						/>
+					</RadioButtonGroup>
+
+					<InputComponent/>
+
 					<RaisedButton label="Cool Button"/>
 				</div>
 			</MuiThemeProvider>
